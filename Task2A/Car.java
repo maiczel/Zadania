3f4f230798ffecac29184ea8b7165ece503732b9
Tomasz Machaj <t.machaj@outlook.com>
2022-02-09 21:52:18 +0100

    Task2A

---
+++ Task2A/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    public String name;
+    public int maxSpeed;
+
+    public Car() {
+        name = "Auto";
+    }
+
+
+    public void printName() {
+
+        System.out.println(name);
+    }
+
+}
+
